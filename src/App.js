d066a9a39fbcf3878a2958146e0531113328f6e3
marc-alzapiedi <marcalzapiedi@gmail.com>
2023-05-09 19:15:19 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import { Repos } from './components/Repos'
+import { MessageBoard } from './components/MessageBoard'
 
 function App() {
 
   return (
-    <Repos />
+    <MessageBoard />
   );
 }
 
