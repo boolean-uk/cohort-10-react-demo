80d27c68085c697a945855adaf1bb6c6c3209708
dearshrewdwit <dearshrewdwit@gmail.com>
2023-05-10 08:46:52 +0100

    handle not found simply

--- src/components/Repos/Repos.js
+++ src/components/Repos/Repos.js
@@ -5,15 +5,18 @@ import { useState, useEffect } from 'react'
 // show also a input field where i can type any user's github username
 // when i hit the button, i want to see their repositories on the page.
 
+// clicking a repo should navigate to /user/repo-name and show information about the repo
+// I should be able to navigate back to the home page showing a list of repositories for the user
+
 const initialFormData = {
   github: 'dearshrewdwit'
 }
 
 function Repos () {
-
   const [repos, setRepos] = useState([])
   const [username, setUsername] = useState('dearshrewdwit')
   const [formData, setFormData] = useState(initialFormData)
+  const [notFound, setNotFound] = useState(false)
 
   useEffect(() => {
     // https://api.github.com/users/${username}/repos
@@ -21,8 +24,9 @@ function Repos () {
       .then(res => res.json()) // read the response format which is stored in JSON
       .then(data => {
       if (data.message === 'Not Found') {
-
+        setNotFound(true)
       } else {
+        setNotFound(false)
         setRepos(data)
       }
     })
@@ -39,6 +43,9 @@ function Repos () {
 
   return (
     <>
+      {
+        notFound && <div>user '{username}' does not exist</div>
+      }
       <form onSubmit={handleSubmit}>
         <input type="text" name="github" onChange={handleChange} value={formData.github}/>
         <button>get repos</button>
