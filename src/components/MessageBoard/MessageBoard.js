6b38415faa341cac90f5a65822a311dc98a0d322
Ian Grantham <dev.iangrantham@gmail.com>
2023-05-10 09:29:22 +0100

    complete for update and delete

--- src/components/MessageBoard/MessageBoard.js
+++ src/components/MessageBoard/MessageBoard.js
@@ -34,6 +34,7 @@ const changeAuthor = (e) => {
       content,
       author,
       heard: false,
+      id: Math.random()
     }
 
   const options = {
@@ -49,27 +50,45 @@ const changeAuthor = (e) => {
     setMessages([...messages, newMessage])
   }
 
-//update this handler
+// update this handler
 
-  // const handleDelete = (message) => {
-  //   const newMessages = messages.filter(item => {
-  //     if (item !== message) {
-  //       return message
-  //     }
-  //   })
-  //   setMessages(newMessages)
-  //   const opts = {
-  //     method: 'DELETE'
-  //   }
-  //   fetch(`http://localhost:4000/messagesData/`, opts)
-  //   .then(function (response) {
-  //     return response.text()
-  //   }
+  const handleDelete = (message) => {
+    const newMessages = messages.filter(item => {
+      if (item !== message) {
+        return message
+      }
+    })
+    setMessages(newMessages)
+    const opts = {
+      method: 'DELETE'
+    }
+    fetch(`http://localhost:4000/messagesData/${message.id}`, opts)
+    .then(res => res.text())
+    }
 
-  // }
 
 //update this handler
 
+  const handleUpdate = (message,value) => {
+    const newMessages = messages.map(item => {
+      if (item === message) {
+        return {
+          ...item,
+          heard: value
+        }
+      } else {
+          return item
+      }
+    })
+    const opts = {
+      method: 'PATCH',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({heard:value})
+    }
+    fetch(`http://localhost:4000/messagesData/${message.id}`, opts)
+    .then(res => res.text())
+    setMessages(newMessages)
+  }
 
 
 //check render is pointing to correct functions
@@ -95,8 +114,8 @@ const changeAuthor = (e) => {
             <Message
               key={index}
               message={messageObj}
-              // handleDelete={handleDelete}
-              // handleUpdate={handleUpdate}
+              handleDelete={handleDelete}
+              handleUpdate={handleUpdate}
             />
           )
         })
